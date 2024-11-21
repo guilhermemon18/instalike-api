056a074757ba04ea38dd80ac0a021f121316b125
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-11-21 11:42:00 -0300

    Criando rotas post/post e post/upload com multer

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,30 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import {
+  listarPosts,
+  postarNovoPost,
+  uploadImagem,
+} from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
+  //rota para buscar todos os posts
   app.get("/posts", listarPosts);
+  //rota para criar um post
+  app.post("/posts", postarNovoPost);
+  //middleware, pequeno programa que vai no meio da requisição (Multer) para enviar imagens:
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
