5b73ea5ac1789b47542cfdeb7515f550b407eb07
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-11-22 11:00:39 -0300

    Integração como API do gemini, atualizar post(PUT),configuração deploy

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,10 @@
 import fs from "fs";
-import { criarPost, getTodosPosts } from "../models/postsModel.js";
+import {
+  atualizarPost,
+  criarPost,
+  getTodosPosts,
+} from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiService.js";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
@@ -34,5 +39,29 @@ export async function uploadImagem(req, res) {
   } catch (erro) {
     console.error(erro.message);
     res.status(500).json({ Erro: "Falha na requisição" });
+  }
+}
+
+export async function atualizarNovoPost(req, res) {
+  const id = req.params.id;
+  const urlImagem = `http://localhost:3000/${id}.png`;
+
+  try {
+    const imgBuffer = fs.readFileSync(`uploads/${id}.png`);
+    const descricao = await gerarDescricaoComGemini(imgBuffer);
+
+    const post = {
+      imgUrl: urlImagem,
+      descricao: descricao,
+      alt: req.body.alt,
+    };
+
+    const postCriado = await atualizarPost(id, post);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({
+      Erro: "Falha na requisição",
+    });
   }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,17 @@
+import cors from "cors";
 import express from "express";
 import multer from "multer";
 import {
+  atualizarNovoPost,
   listarPosts,
   postarNovoPost,
   uploadImagem,
 } from "../controllers/postsController.js";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200,
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -19,12 +26,15 @@ const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
+  app.use(cors(corsOptions));
   //rota para buscar todos os posts
   app.get("/posts", listarPosts);
   //rota para criar um post
   app.post("/posts", postarNovoPost);
   //middleware, pequeno programa que vai no meio da requisição (Multer) para enviar imagens:
   app.post("/upload", upload.single("imagem"), uploadImagem);
+
+  app.put("/upload/:id", atualizarNovoPost);
 };
 
 export default routes;
